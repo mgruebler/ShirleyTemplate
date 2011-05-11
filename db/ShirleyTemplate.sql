32b607c5c7ec212a2e09fff531e63874ab84a4a1
Martin Kloesch <martin.kloesch@student.tugraz.at>
2011-05-11 11:22:48 +0200

    DB: Prototyp [CP,KS]

---
+++ db/ShirleyTemplate.sql
@@ -0,0 +1,36 @@
+
+CREATE DATABASE `shirleytemplate` /*!40100 DEFAULT CHARACTER SET utf8 */
+CREATE TABLE `Files` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `templateID` int(10) unsigned NOT NULL,
+  `data` blob NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
+
+CREATE TABLE `Template` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(10) unsigned NOT NULL,
+  `type` varchar(10) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `userID_UNIQUE` (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
+
+CREATE TABLE `User` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Username_UNIQUE` (`username`),
+  UNIQUE KEY `ID_UNIQUE` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `UserTemplateData` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `userID` int(10) unsigned NOT NULL,
+  `data` varchar(45) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
