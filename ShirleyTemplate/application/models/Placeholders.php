3c00955685b79ce223d8c27e588de0020b027725
Marco Gruebler <mgruebler@student.tugraz.at>
2011-05-25 10:07:38 +0200

    created placeholders db [MG, KS]

---
+++ ShirleyTemplate/application/models/Placeholders.php
@@ -0,0 +1,80 @@
+<?php
+
+class Application_Model_Placeholders
+{
+    protected $_ID;
+    protected $_name;
+    protected $_tamplateID;
+
+    public function __construct(array $options = null)
+    {
+        if (is_array($options)) {
+            $this->setOptions($options);
+        }
+    }
+
+    public function __set($name, $value)
+    {
+        $method = 'set' . $name;
+        if (('mapper' == $name) || !method_exists($this, $method)) {
+            throw new Exception('Invalid placeholder property');
+        }
+        $this->$method($value);
+    }
+
+    public function __get($name)
+    {
+        $method = 'get' . $name;
+        if (('mapper' == $name) || !method_exists($this, $method)) {
+            throw new Exception('Invalid placeholder property');
+        }
+        return $this->$method();
+    }
+
+    public function setOptions(array $options)
+    {
+        $methods = get_class_methods($this);
+        foreach ($options as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (in_array($method, $methods)) {
+                $this->$method($value);
+            }
+        }
+        return $this;
+    }
+    
+    public function setID($id)
+    {
+        $this->_ID = (int) $id;
+        return $this;
+    }
+
+    public function getID()
+    {
+        return $this->_ID;
+    }
+    
+    public function setTemplateID($id)
+    {
+        $this->_templateID = (int) $id;
+        return $this;
+    }
+
+    public function getTemplateID()
+    {
+        return $this->_templateID;
+    }
+
+    public function setName($name)
+    {
+        $this->_name = (string) $name;
+        return $this;
+    }
+
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+}
+
