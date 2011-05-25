08237312715ff9ecdbd2ca1c0accf13d8820b8e3
Bernd Hirschmann <bernd@hirschmann.st>
2011-05-25 11:14:43 +0200

    fixed DownloadController updated DownloadControllerTest

--- ShirleyTemplate/application/controllers/DownloadController.php
+++ ShirleyTemplate/application/controllers/DownloadController.php
@@ -4,31 +4,19 @@ class DownloadController extends Zend_Controller_Action
 {
     public function init()
     {
-     
-
-
     }
 
     public function indexAction()
     {	
     	$_SESSION['DownloadFileName'] = "files/test1.zip";
-    	
-    	$this->view->DownloadFileName = $_SESSION['DownloadFileName'];
-    	
-    //if (userHasNoPermissions) {
-    //    $this->view->msg = 'This file cannot be downloaded!';
-    //    $this->_forward('error', 'download');
-    //	return false;
-    //} 	
     
     	$response = $this->getResponse();
 
 		$response->setHeader('Content-Type', 'application/zip', true);
 		$response->setHeader('Content-Disposition', 'attachment; filename="' . $_SESSION['DownloadFileName'] . '"', true);
-		//$response->setHttpResponseCode(404);
-		//readfile($_SESSION['DownloadFileName']);
+		readfile($_SESSION['DownloadFileName']);
 		
-		// disable the view ... and perhaps the layout
+		// disable the view
 		$this->_helper->layout->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
     }
--- ShirleyTemplate/tests/application/controllers/DownloadControllerTest.php
+++ ShirleyTemplate/tests/application/controllers/DownloadControllerTest.php
@@ -4,32 +4,22 @@ require_once 'BaseTestCase.php';
 
 class DownloadControllerTest extends ControllerTestCase
 {
-	
-	
     public function testIndexAction()
     {
+    	// todo: generate file and set file name
+    	$_SESSION['DownloadFileName'] = "files/test1.zip";
+    	
     	$front = Zend_Controller_Front::getInstance();
     	$front->setParam('noErrorHandler', true);
         $this->dispatch('/download');
-        
-        //$this->assertHeader($header)
+       
         $this->assertController('download');
         $this->assertAction('index');
         
         $this->assertResponseCode(200);
         
-        $response = $this->getResponse();
-        
-        print('begin response');
-        print($response->__toString());
-        print('end response');
-        
-        foreach ($response->getHeaders() as $header=>$headerdata)
-        {
-        	print('bla');
-        	print($header);
-        	print($headerdata);
-        }
+        $this->assertHeaderContains('Content-Type', 'application/zip');
+        $this->assertHeaderContains('Content-Disposition', 'attachment; filename="files/test1.zip"');
     }
 }
 
